07077fa005c0f4dbb367a8bd1af60de37dea3190
shulksteve <shulk.work@gmail.com>
2024-12-24 12:02:44 +0800

    update: build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -3,9 +3,9 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
   entries: ['src/index'],
   outDir: 'dist',
-  declaration: true, // 生成 TypeScript 类型声明文件
-  clean: true, // 清理输出目录
+  declaration: true,
+  clean: true,
   rollup: {
-    emitCJS: false, // 仅生成 ESM 格式
+    emitCJS: false,
   },
 });
